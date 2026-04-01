eb33504a54f43f98c2fc4b9c1c22ccc6d7272d7f
Shadman Chowdhury <shadmanchowdhury01@gmail.com>
2026-03-31 23:08:24 -0400

    third problem revolving around reordering an array

--- Arrays/practice1.js
+++ Arrays/practice1.js
@@ -12,7 +12,7 @@ const rearrange = (arr) => {
     return results;
 }
 
-console.log(rearrange([-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]));
+// console.log(rearrange([-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]));
 
 // this approach rather than rearranging the original array in place returns a new array. Need to revisit to figure out a algorithm that will meet the conditions of the problem
 
@@ -29,4 +29,22 @@ const maxSum = (arr) => {
     return;
 }
 
-maxSum([8, -8, 9, -9, 10, -11, 12])
+// maxSum([8, -8, 9, -9, 10, -11, 12])
+
+// this approach is different from the other approaches in that I am using a map to store the index and the value
+// in my second iteration I am updating the spots within the array
+const pairOrdering = (arr, ind) => {
+    const pairs = {};
+    for(let i = 0; i < arr.length; i++) {
+        pairs[arr[i]] = ind[i];
+    }
+    for (const [key, value] of Object.entries(pairs)) {
+        const keyNum = Number(key);
+        arr[value] = keyNum;
+    }
+    console.log(arr);
+    return arr;
+}
+
+// pairOrdering([10,11,12], [1,0,2]);
+// pairOrdering([50, 40, 70, 60, 90], [3,  0,  4,  1,  2])
