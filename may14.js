597a85c0bb431a187d0a49d77616d7d12561fc2a
Shadman Chowdhury <shadmanchowdhury01@gmail.com>
2026-05-28 21:53:50 -0400

    reduce

--- may14.js
+++ may14.js
@@ -2,39 +2,54 @@
 // arr.myFilter()
 // 
 
-Array.prototype.myFilter = function(x) {
-// use 'this' to access the array
-    if(!x) throw new Error('This function expects at least one argument');
+Array.prototype.myFilter = function (x) {
+    // use 'this' to access the array
+    if (!x) throw new Error('This function expects at least one argument');
     const results = [];
-    for(let i = 0; i < this.length; i++) {
-        if(x(this[i])) results.push(this[i]);
+    for (let i = 0; i < this.length; i++) {
+        if (x(this[i])) results.push(this[i]);
     }
-    return results;    
+    return results;
 }
 
-const nums1 = [1,2,3,4,5,6,7,8,9,10];
+const nums1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(nums1.myFilter(x => x % 2 === 0));
 
-Array.prototype.myMap = function(x) {
-    if(!x) throw new Error('This function expects at least one argument');
+Array.prototype.myMap = function (x) {
+    if (!x) throw new Error('This function expects at least one argument');
     const results = [];
-    for(let i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         results.push(x(this[i]));
     }
     return results;
 }
 
-Array.prototype.myMap = function(x) {
-    if(!x) throw new Error('This function expects at least one argument');
-    const results = [];
-    for(let i = 0; i < this.length; i++) {
-        results.push(x(this[i]));
+Array.prototype.myReduce = function (callback, initialValue) {
+    // if(!x) throw new Error('This function expects at least one argument');
+    if (typeof callback !== "function") {
+        throw new TypeError("Callback must be a function");
     }
-    return results;
+    let accumulator;
+    let startIndex;
+
+    if (initialValue !== undefined) {
+        accumulator = initialValue;
+        startIndex = 0;
+    } else {
+        if (this.length === 0) {
+            throw new TypeError("no initial value");
+        }
+        accumulator = this[0];
+        startIndex = 1;
+    }
+    for (let i = startIndex; i < this.length; i++) {
+        accumulator = callback(accumulator, this[i], i, this);
+    }
+    return accumulator;
 }
 console.log(nums1.myMap(x => x * 2));
-
+console.log(nums1.myReduce());
 //figure out the difference between regular functions and arrow functions
 // Homework: create myMap, myReduce, myFlatMap, mySort(use any sorting algorithm), any other functions
 // read book to grasp basics of JS, Mozilla, you don't know JS
