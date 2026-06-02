dbbe7d5668b3177e4450bbcef1152ad3798a90f8
Shadman Chowdhury <shadmanchowdhury01@gmail.com>
2026-06-01 23:03:53 -0400

    leetcode 160 done

---
+++ LinkedLists/Leetcode160.js
@@ -0,0 +1,12 @@
+var getIntersectionNode = function(headA, headB) {
+    let pointerA = headA;
+    let pointerB = headB;
+
+    while(pointerA !== pointerB) {
+        if(pointerA === null) pointerA = headB;
+        else pointerA = pointerA.next;
+        if(pointerB === null) pointerB = headA;
+        else pointerB = pointerB.next;
+    }
+    return pointerA;
+};
